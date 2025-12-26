64b1560b1bd4709f2afb8cfb4efc17d443074e93
cnavidad <cnavidad93@gmail.com>
2025-12-26 16:34:14 +0100

    open cors

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const { injectWebSocket, upgradeWebSocket } = createNodeWebSocket({ app });
 app.use(
   "*",
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: "*",
     allowMethods: ["GET", "POST", "OPTIONS"],
     allowHeaders: ["Content-Type"],
   })
